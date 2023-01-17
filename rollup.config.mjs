ea94e95bbbcad9c8db433bdb0ff653c856d308d5
zephyr <zzzzzzzephyr@gmail.com>
2023-01-17 14:48:00 +0800

    es module

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -16,6 +16,19 @@ export default {
       }
       return path
     }
+  }, {
+    name: 'y-websocket',
+    file: 'dist/y-websocket.mjs',
+    format: 'es',
+    sourcemap: true,
+    // paths: path => {
+    //   if (/^lib0\//.test(path)) {
+    //     return `lib0/dist${path.slice(4)}.cjs`
+    //   } else if (/^y-protocols\//.test(path)) {
+    //     return `y-protocols/dist${path.slice(11)}.cjs`
+    //   }
+    //   return path
+    // }
   }],
   plugins: [typescript()]
 }
